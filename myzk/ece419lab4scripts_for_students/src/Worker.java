88b05281270861d4f573b6e8b75091e4d3571246
Saad <saads.saleem@mail.utoronto.ca>
2016-03-25 15:03:08 -0400

    creating worker class

--- myzk/ece419lab4scripts_for_students/src/Worker.java
+++ myzk/ece419lab4scripts_for_students/src/Worker.java
@@ -15,15 +15,47 @@ public class Worker{
 
 	static Socket fileServerSocket = null;
 	static ZkConnector zkc = null;
+	static String FSpath = "/fileserver";
 	static String jobPath = "/jobs";
 	static String finishedPath = "/finished";
 	static Watcher watcher = null;
 	static String hash = null;
 
-	public Worker;
+	public Worker(String hosts) {
+		zkc = new ZkConnector();
+		try {
+		  zkc.connect(hosts);
+		} catch(Exception e) {
+		  System.out.println("Zookeeper connect "+ e.getMessage());
+		}
+
+		watcher = new Watcher() { // Anonymous Watcher
+		    @Override
+		    public void process(WatchedEvent event) {
+		      handleEvent(event);
+		                    
+		    }
+		};
+		
+	}
+
+	private static void handleEvent(WatchedEvent event) {
+		String path = event.getPath();
+		System.out.println(path);
+		EventType type = event.getType();
+		if (path.equalsIgnoreCase(myPath)) {
+
+		}
+	}
 
 
-	 public static void main (String[] args){
+	public static void main (String[] args){
+
+		if(args.length != 1) {
+			System.out.println("Usage: java -classpath lib/zookeeper-3.3.2.jar:lib/log4j-1.2.15.jar:. WorkerPool zkServer:clientPort\n");
+      		return;
+		}
 	 	
+		Worker worker = new worker(args[0]);
 
 }
